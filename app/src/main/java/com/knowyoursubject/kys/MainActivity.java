4dae2d2c03f972c87752bced1854f86789dc8b40
Andrey <andrey@tstoryteller.com>
2023-02-23 12:30:01 +0200

    start page: button clicks

--- app/src/main/java/com/knowyoursubject/kys/MainActivity.java
+++ app/src/main/java/com/knowyoursubject/kys/MainActivity.java
@@ -2,13 +2,41 @@ package com.knowyoursubject.kys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View btnStart = findViewById(R.id.btnStart);
+        btnStart.setBackgroundColor(Color.rgb(0, 255, 0));
+        btnStart.setOnClickListener(this);
+
+        View btnMore = findViewById(R.id.btnMore);
+        btnMore.setBackgroundColor(Color.rgb(0, 255, 0));
+        btnMore.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btnStart){
+            //define a new Intent for the second Activity
+            //Intent intent = new Intent(this,StartActivity.class);
+            //start the second Activity
+            //this.startActivity(intent);
+        }
+        if(view.getId() == R.id.btnMore){
+            String url = "http://www.knowyoursubject.com";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        }
     }
 }
